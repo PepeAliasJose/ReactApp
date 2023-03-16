d17ae996dc552f5ced104839803f23260054ab2f
PepeAliasJose <peperc781@gmail.com>
2023-03-17 00:44:41 +0100

    Hasta video 23
    React Router
    Exact match routes
    Router links

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,31 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar/> {/*Componente navbar creado antes*/}
-      <div className="content">
-        <Home/> {/*Componente home*/}
+    <Router> {/*Componente para router*/}
+      <div className="App">
+        <Navbar/> {/*Componente navbar creado antes*/}
+        <div className="content">
+          <Switch> {/*Componente que envuelve los posibles contenidos*/}
+
+            {/*La ruta de / va la ultima por que hace match*/}
+            {/* O usas el exact -> */}
+            <Route exact path='/'> 
+              <Home/> 
+            </Route>
+
+            <Route path='/create'> {/* Si la ruta acccedida es la del path pinta el componente*/}
+              <Create/>
+            </Route>
+
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- reactapp/src/Navbar.js
+++ reactapp/src/Navbar.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The dojo blog </h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link> {/*Si pones el componente Link lo intercepta React 
+                para inyectar contenido en el DOM antes de hacer una solicitud al servidor*/}
+                <Link to="/create" style={{
                     color:'white' ,
                     backgroundColor: '#f1356d',
                     borderRadius: '8px'
-                }}>Nuevo blog</a>
+                }}>Nuevo blog</Link>
             </div>
         </nav>
     );
